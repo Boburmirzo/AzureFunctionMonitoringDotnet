7ddc98df19d8f81184bb4d1474e83d3522aab45a
Livermore <boburmirzo.umurzokov@gmail.com>
2021-10-13 15:52:11 +0300

    Add files via upload

---
+++ AzureFunctionHealth/src/FunctionHealthCheck/HealthChecksBuilder.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionHealthCheck
+{
+    internal class HealthChecksBuilder : IHealthChecksBuilder
+    {
+        public HealthChecksBuilder(IServiceCollection services)
+        {
+            Services = services;
+        }
+
+        public IServiceCollection Services { get; }
+
+        public IHealthChecksBuilder Add(HealthCheckRegistration registration)
+        {
+            if (registration == null)
+            {
+                throw new ArgumentNullException(nameof(registration));
+            }
+
+            Services.Configure<HealthCheckServiceOptions>(options =>
+            {
+                options.Registrations.Add(registration);
+            });
+
+            return this;
+        }
+    }
+}
